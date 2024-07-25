e1c9dbdd688d941e4fa7d4b37289e80251e8da36
Vladimir <vladimir156xz@gmail.com>
2024-07-26 01:59:32 +0200

    deleted trash files

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/flashStyles.css'
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
