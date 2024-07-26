e0a9ff17757da215b3e4a69a0ac96f1d038677bc
Vladimir <vladimir156xz@gmail.com>
2024-07-26 14:39:57 +0200

    without confirmation page

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -14,8 +14,6 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|View
     {
-        return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
-                    : view('auth.verify-email');
+        return redirect()->intended(route('/'));
     }
 }
