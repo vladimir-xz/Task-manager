8236283c47deb0fd693f131cf8a865000eca9b19
Vladimir <vladimir156xz@gmail.com>
2024-07-27 22:20:54 +0200

    deleted unnessesary code to include csrf tokens

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,15 +4,3 @@ window._ = _;
 
 import axios from 'axios';
 window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    console.info('CSRF has been connected');
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-    setTimeout(function() { document.getElementsByClassName('popover-background-window')[0].click(); }, 100);
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="csrf-param" content="_token" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Менеджер задач') }}</title>
 
