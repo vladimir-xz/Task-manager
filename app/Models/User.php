d8dc1ba40593c337874300a32804a436dae11381
Vladimir <vladimir156xz@gmail.com>
2024-07-27 19:37:44 +0200

    Asset Bundling (Vite) config first try

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory;
-    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +22,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
         Gate::define('delete-task', function (User $user, Task $task) {
             // Log::info("userId: {$user->id} taskid {$task->created_by_id}");
             return $user->id === $task->created_by_id;
