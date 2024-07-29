be64fa879f3fb5be152603c08733d0f32b1a45a6
Vladimir <vladimir156xz@gmail.com>
2024-07-29 23:48:13 +0200

    make div attributes in label form separate variable

--- resources/views/labels/form.blade.php
+++ resources/views/labels/form.blade.php
@@ -1,14 +1,18 @@
-<div class="mt-2">
+@php
+    $divAttributes = "class=mt-2";
+@endphp
+
+<div $divAttributes>
     {{  html()->label(__('Name'), 'name')->class('block font-medium text-sm text-gray-700 dark:text-gray-300') }}
 </div>
-<div class="mt-2">
+<div $divAttributes>
     {{  html()->input('text', 'name')->class('w-1/3 rounded border-gray-300')}}
 </div>
 <x-input-error :messages="$errors->get('name')" class="mt-2" />
-<div class="mt-2">
+<div $divAttributes>
     {{  html()->label(__('Description'), 'description')->class('block font-medium text-sm text-gray-700 dark:text-gray-300') }}
 </div>
-<div class="mt-2">
+<div $divAttributes>
     {{  html()->textarea('description')->class('w-1/3 rounded border-gray-300')}}
 </div>
 <x-input-error :messages="$errors->get('description')" class="mt-2" />
