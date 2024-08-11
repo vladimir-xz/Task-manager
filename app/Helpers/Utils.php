0a53ec18bb190a39c79764a8137c758cfc3cf742
Vladimir <vladimir156xz@gmail.com>
2024-08-11 22:30:43 +0200

    another problem with template type

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Collection;
 
 class Utils
 {
+    /**
+     * @template T
+     * @param T $collection
+     * @return T
+     */
     public static function groupByIdWithName(Collection $collection): array
     {
         return $collection->mapWithKeys(function (object $record) {
