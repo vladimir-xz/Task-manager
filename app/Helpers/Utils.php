9a0a096104f64d0c6e64cc141e32f439b4cf25d7
Vladimir <vladimir156xz@gmail.com>
2024-08-11 23:21:07 +0200

    deleted all generics

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -4,15 +4,12 @@ namespace App\Helpers;
 
 use Illuminate\Database\Eloquent\Collection;
 
-// Illuminate\Database\Eloquent\Collection
-
 class Utils
 {
     public static function groupByIdWithName(Collection $collection): array
     {
-        /** @var Collection<int, Illuminate\Database\Eloquent\Model> $collection */
         return $collection->mapWithKeys(function (\stdClass $record) {
-            return [$record?->id => $record?->name];
+            return [$record->id => $record->name];
         })->all();
     }
 }
