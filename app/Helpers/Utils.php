502a5ce5505c5a5c3775fc9035eb50ce2aa0e55a
Vladimir <vladimir156xz@gmail.com>
2024-08-11 23:55:48 +0200

    trying to include every object type in groupIdByNames

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -2,13 +2,16 @@
 
 namespace App\Helpers;
 
+use App\Models\Label;
+use App\Models\Task;
+use App\Models\TaskStatus;
 use Illuminate\Database\Eloquent\Collection;
 
 class Utils
 {
     public static function groupByIdWithName(Collection $collection): array
     {
-        return $collection->mapWithKeys(function (mixed $record) {
+        return $collection->mapWithKeys(function (Task|TaskStatus|Label $record) {
             return [$record->id => $record->name];
         })->all();
     }
