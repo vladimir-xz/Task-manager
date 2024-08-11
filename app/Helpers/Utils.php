a1214744345598ec19c488bb2ca51d8e68d0825b
Vladimir <vladimir156xz@gmail.com>
2024-08-11 23:57:26 +0200

    forget to include Usert type

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -5,13 +5,14 @@ namespace App\Helpers;
 use App\Models\Label;
 use App\Models\Task;
 use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
 class Utils
 {
     public static function groupByIdWithName(Collection $collection): array
     {
-        return $collection->mapWithKeys(function (Task|TaskStatus|Label $record) {
+        return $collection->mapWithKeys(function (Task|TaskStatus|Label|User $record) {
             return [$record->id => $record->name];
         })->all();
     }
