ea6b38948b441f5bbe94880baeee0ad5be9a3ae6
Vladimir <vladimir156xz@gmail.com>
2024-08-11 23:17:29 +0200

    use only one line of generics

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -6,16 +6,12 @@ use Illuminate\Database\Eloquent\Collection;
 
 // Illuminate\Database\Eloquent\Collection
 
-
 class Utils
 {
-    /**
-     * @param T $collection
-     */
     public static function groupByIdWithName(Collection $collection): array
     {
         /** @var Collection<int, Illuminate\Database\Eloquent\Model> $collection */
-        return $collection->mapWithKeys(function (object $record) {
+        return $collection->mapWithKeys(function (\stdClass $record) {
             return [$record?->id => $record?->name];
         })->all();
     }
