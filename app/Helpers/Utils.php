e3892f0ec2755e05ba2e2a9ec6ed743c46700509
Vladimir <vladimir156xz@gmail.com>
2024-08-12 00:04:02 +0200

    another attempt, using model in mapwithkeys function

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -7,12 +7,13 @@ use App\Models\Task;
 use App\Models\TaskStatus;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class Utils
 {
     public static function groupByIdWithName(Collection $collection): array
     {
-        return $collection->mapWithKeys(function (Task|TaskStatus|Label|User $record) {
+        return $collection->mapWithKeys(function (Model $record) {
             return [$record->id => $record->name];
         })->all();
     }
