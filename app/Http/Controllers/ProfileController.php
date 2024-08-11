3ee2866f16da263f2b3e6470dbb4f547162cfe5c
Vladimir <vladimir156xz@gmail.com>
2024-08-11 22:15:05 +0200

    second attempt

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -44,7 +44,7 @@ class ProfileController extends Controller implements HasMiddleware
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
-            if ($request->user()?->email_verified_at) {
+            if (!is_null($request->user()?->email_verified_at)) {
                 $request->user()->email_verified_at = null;
             }
         }
