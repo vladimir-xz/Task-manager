21b27da64d3582fcfceb75c9011063f92bd55f3a
Vladimir <vladimir156xz@gmail.com>
2024-08-13 01:00:04 +0200

    another attempt to solve the same problem

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -17,6 +17,7 @@ class Utils
      */
     public static function groupByIdWithName(Collection $collection): array
     {
+        /** @var \Illuminate\Support\Collection $collection */
         return $collection->mapWithKeys(function (Model $record) {
             return [$record->id => $record->name];
         })->all();
