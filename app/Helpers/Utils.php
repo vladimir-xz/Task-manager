5b430a502847465a14f82622ba474a86641cb5c6
Vladimir <vladimir156xz@gmail.com>
2024-08-13 00:49:36 +0200

    deleted all specified types

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 class Utils
 {
     /**
-     * @param Collection<int, Label|Task|TaskStatus|User> $collection
+     * @param Collection $collection
      * @return array<int, string>
      */
     public static function groupByIdWithName(Collection $collection): array
