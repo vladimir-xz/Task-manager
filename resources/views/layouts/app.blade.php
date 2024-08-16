8972dd9e9be2863739410192fca2489357caad03
vladimir-xz <vladimir156xz@gmail.com>
2024-08-17 00:14:41 +0200

    solution to not shrinking menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,12 @@
     </head>
     <body class="font-sans antialiased min-h-screen" id="app">
 
-        <header class="fixed w-full">
+        <header class="fixed w-full sm:mt:10">
             @include('layouts.navigation')
         </header>
         <!-- Page Heading -->
         <section class="bg-white dark:bg-gray-900 min-h-screen ">
-            <div class="grid max-w-screen-xl px-4 pt-20 sm:pt-60 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+            <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
                 @include('flash::message')
                     <div class="grid col-span-full">
                         @isset($header)
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,14 +1,14 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 py-2.5 shadow-md">
     <!-- Primary Navigation Menu -->
-    <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                 <!-- Logo -->
                 <a class="flex items-center" href="{{ route('dashboard') }}">
                     <x-application-logo class="text-xl text-gray-800 dark:text-gray-200" />
                 </a>
 
                 <!-- Navigation Links -->
-                <div class="items-center justify-between w-full lg:flex lg:w-auto lg:order-1 hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <div class="items-center justify-between w-full flex lg:w-auto lg:order-1 space-x-8 -my-px ms-10 ">
+                    <ul class="flex flex-row ms-4 font-medium space-x-8">
                         <li>
                             <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
                                 {{ __('Tasks') }}
