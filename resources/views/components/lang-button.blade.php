0bacff573cc356e5a6eb8edfe48da9b0bcec3a9c
Vladimir <vladimir156xz@gmail.com>
2024-08-17 17:26:22 +0200

    constantly showing current lanuage

---
+++ resources/views/components/lang-button.blade.php
@@ -0,0 +1,27 @@
+@props(['lang'])
+
+@php
+$locale = App::getLocale();
+
+$language = match ($locale) {
+    'eng' => __('English'),
+    'ru' => __('Russian'),
+    default => __('English'),
+}
+@endphp
+
+<button {{ $attributes->merge([
+        'id' => 'dropdownHoverButton',
+        'data-dropdown-toggle' => 'dropdownHover',
+        'data-dropdown-trigger' => 'hover',
+        'class' => 'text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center inline-flex items-center dark:hover:bg-blue-700 dark:focus:ring-blue-800',
+        'type' => 'button'
+    ]) }}
+    >
+
+    {{ $language }} 
+
+    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+    </svg>
+</button>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,12 +30,8 @@
 
                 <div class="flex flex-inline items-center justify-between">
                     <div class="flex flex-inline p-5">
-                        
-                        <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center inline-flex items-center dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{{ __('Language')}} <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                        </svg>
-                        </button>
 
+                        <x-lang-button></x-lang-button>
                         <!-- Dropdown menu -->
                         <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                             <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
