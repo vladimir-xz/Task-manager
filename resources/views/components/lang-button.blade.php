61c9e2978789ce296177a3f2f93606eb836450a5
Vladimir <vladimir156xz@gmail.com>
2024-08-17 17:53:59 +0200

    changed the padding distance of language menu

--- resources/views/components/lang-button.blade.php
+++ resources/views/components/lang-button.blade.php
@@ -14,7 +14,7 @@ $language = match ($locale) {
         'id' => 'dropdownHoverButton',
         'data-dropdown-toggle' => 'dropdownHover',
         'data-dropdown-trigger' => 'hover',
-        'class' => 'text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center inline-flex items-center dark:hover:bg-blue-700 dark:focus:ring-blue-800',
+        'class' => 'text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-blue-700 dark:focus:ring-blue-800',
         'type' => 'button'
     ]) }}
     >
