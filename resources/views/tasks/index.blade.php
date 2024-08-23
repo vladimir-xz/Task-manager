b97fdc80d11ef64036ba1048d6a39860b585d028
Vladimir <vladimir156xz@gmail.com>
2024-08-23 18:57:56 +0200

    working list view of tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,3 +1,4 @@
+
 @php
     $selectAttr = "class= rounded border-gray-300 dark:border-gray-700 dark:bg-black dark:text-neutral-200";
 @endphp
@@ -8,7 +9,7 @@
         {{ __('Tasks')}}
     </x-header>
 
-    <div class="flex w-full items-center justify-between">
+    <div class="flex flex-wrap w-full items-center justify-between">
         {{  html()->form('GET', route('tasks.index'))->class('flex flex-wrap gap-x-1 gap-y-5')->open() }}
                 {{  html()->select('filter[status_id]', $statusesByIds, $filter['status_id'] ?? 0)->class($selectAttr)->placeholder(__('Status'))    }}
                 {{  html()->select('filter[created_by_id]', $usersByIds, $filter['created_by_id'] ?? 0)->class($selectAttr)->placeholder(__('Author'))    }}
@@ -29,7 +30,7 @@
     </div>
 
     <table class="mt-4 dark:text-neutral-200">
-        <thead class="border-b-2 border-solid border-black text-left">
+        <thead class="hidden md:table-header-group border-b-2 border-solid border-black text-left">
             <tr>
                 <th>ID</th>
                 <th>{{ __('Status') }}</th>
@@ -44,19 +45,21 @@
         </thead>
         <tbody>
             @foreach ($tasks as $task)
-                <tr class="border-b border-dashed text-left">
-                    <td>{{  $task->id }}</td>
-                    <td>{{  $task->status->name   }}</td>
-                    <td>
+                <tr class="flex flex-wrap justify-between md:table-row flex-initial w-full md:border-b md:border-dashed text-left">
+                    <td class="order-1 flex-none w-10">{{  $task->id }}</td>
+                    <td class="order-3 c">{{  $task->status->name   }}</td>
+                    <td class="order-2 basis-11/12 max-w-full" >
                         <a href="{{ route('tasks.show', $task->id)}}" class="text-blue-700 hover:text-indigo-400">
-                            <x-short-inscription>
-                                {{  $task->name }}
-                            </x-short-inscription>
+                            {{  $task->name }}
                         </a></td>
-                    <td>{{  $task->author->name }}</td>
-                    <td>{{  $task->assignedTo?->name }}</td>
-                    <td>{{  $task->created_at->format('d.m.Y') }}</td>       
-                    <td>                 
+                    <td class="order-5 basis-full">{{  $task->author->name }}</td>
+                    <td class="order-4 basis-full"> 
+                        @if ($task->assignedTo?->name)
+                        {{  __('Assigned to: ') . $task->assignedTo?->name }}
+                        @endif
+                    </td>
+                    <td class="order-6 flex-auto w-64">{{  $task->created_at->format('d.m.Y')  }}</td>       
+                    <td class="order-7">                 
                         @can('delete', $task)
                             <a data-confirm="Вы уверены?" data-method="delete" class="text-red-600 hover:text-red-900" href="{{ route('tasks.destroy', $task->id)  }}">
                                 {{ __('Delete') }}
