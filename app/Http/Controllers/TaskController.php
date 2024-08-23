91f0a34bafbdd66fd8c59ce3dfac2b74983a0eca
Vladimir <vladimir156xz@gmail.com>
2024-08-23 20:15:06 +0200

    descending order of records

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -26,6 +26,7 @@ class TaskController extends Controller
                 AllowedFilter::exact('created_by_id'),
                 AllowedFilter::exact('assigned_to_id'),
             ])
+            ->latest('id')
             ->paginate(15);
 
         $statusesByIds = Utils::groupByIdWithName(TaskStatus::all());
