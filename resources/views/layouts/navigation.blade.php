3e02f6fe0dd3ab2b685a5682085aa9501fffc84a
Vladimir <vladimir156xz@gmail.com>
2024-08-24 04:35:33 +0200

    smaller main nav menu on phones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 py-2.5 shadow-md">
     <!-- Primary Navigation Menu -->
-    <div class="flex items-center md:justify-between max-w-screen-xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center md:justify-between max-w-screen-xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
 
 
     <!-- Dropdown menu -->
