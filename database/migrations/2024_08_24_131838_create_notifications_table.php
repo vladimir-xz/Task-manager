895c9a53bfde82bd75d26c8bf62c1e7087165971
Vladimir <vladimir156xz@gmail.com>
2024-08-25 14:25:43 +0200

    renamed table

--- database/migrations/2024_08_24_131838_create_notifications_table.php
+++ database/migrations/2024_08_24_131838_create_notifications_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('task_notification_user');
+        Schema::dropIfExists('task_notifications');
     }
 };
